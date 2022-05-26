3074781f0eaeb2e8fae018fae41dc032a1a9c649
davi12345452 <davijanischmaia@gmail.com>
2022-05-26 17:44:37 -0300

    parte2

--- Início/Primeiro.js
+++ Início/Primeiro.js
@@ -1,3 +1,19 @@
 console.log("Hello World")
 console.log('Olá Mundo');
 // Em js podemos utilizar o ; como opcionais no final da sentença
+// O js permite o usuário dividir o comando ao longo das linhas
+
+console
+.log
+("Bem vindo") //Há alguns problemas, mas são exceções.
+
+// A utilização do ; permite que usemos várias sentenças em uma só linha
+
+console.log("Oi"); console.log("Hello"); //É permitido
+// console.log("Oi") console.log("Hello") -> ERRO
+
+/*
+Seria interessante aderir ao ; em todo final de código para definir
+onde a sentença encerrou, bem como para se acostumar já que várias
+linguagens usuais, como Solidity, C#, C++(cpp) são obrigadas a terminar
+*/
