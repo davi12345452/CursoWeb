6bc6ed693cfdb10bc7334d6c04ff7ae3b3cabb5d
davi12345452 <davijanischmaia@gmail.com>
2022-06-13 17:39:39 -0300

    update

--- Seção 8 - Array JS/metodo_filter.js
+++ Seção 8 - Array JS/metodo_filter.js
@@ -1,3 +1,36 @@
 /* 
- * MÉTODO FILTER:
+ * MÉTODO FILTER: é um método que cria um array, usando um array base e um critério.
+ * Ele filtra dentro de um array, com base em um critério, devolvendo um novo array
+ * com os elementos que cumpriram esses critérios.
  */
+
+const produtos = [
+    {nome: "Iphone" , preço: 5000.00 , fragil: false},
+    {nome: "Copo de Vidro" , preço: 20.00 , fragil: true},
+    {nome: "Televisão" , preço: 2300.00 , fragil: false},
+    {nome: "Lente de Contato" , preço: 230.00 , fragil: true},
+    {nome: "Ipad" , preço: 4000.00 , fragil: false}
+]
+
+// O callback deve retornar true or false, para poder incluir. É como se o critério
+// foi ou não atendido. 
+
+produtos_frageis = produtos.filter(p => p.fragil == true && p.preço <= 220) //Filtra somente os true para frágil e preço menor/igual que 220
+
+console.log(produtos_frageis)
+
+// Criando o próprio filter:
+
+Array.prototype.new_filter = function(callback){
+    const new_Array = []
+    for(let i = 0; this.length > i; i++){
+        if(callback(this[i], i, this) == true){
+            new_Array.push(this[i])
+        }
+    }
+    return new_Array
+}
+
+produto_nfrageis = produtos.new_filter(p => p.fragil == false)
+console.log(produto_nfrageis)
+
--- Seção 8 - Array JS/metodo_reduce.js
+++ Seção 8 - Array JS/metodo_reduce.js
@@ -1,3 +1,47 @@
 /*
- * MÉTODO REDUCE: 
+ * MÉTODO REDUCE: ele pega um por um e realiza operações: [1,2,3,5] -> 1 + 2 = 3 + 3 = 6 + 5...
+ * Utiliza-se um callback sobre um acumulador que vai aumentando a medida que o array é lido
+ * da esquerda para a direita 
  */
+
+const alunos = [
+    {nome: "João" , nota: 7.95 , bolsista: false},
+    {nome: "Maria" , nota: 2.10 , bolsista: false},
+    {nome: "Mateus" , nota: 3.30 , bolsista: true},
+    {nome: "Fabiana" , nota: 10.00 , bolsista: true},
+    {nome: "Marcelo" , nota: 2.40 , bolsista: false}
+]
+
+// Map vai resultar apenas nas notas, reduce vai acumular as notas:
+
+const func_callback = (acumulador, atual) =>{
+    return acumulador + atual //Tem que se atentar, pois, o reduce devolve o último termo e a soma dos anteriores.
+}
+notas_acumuladas = alunos.map(a => a.nota).reduce(func_callback)
+console.log(notas_acumuladas)
+
+// Desafio, todos alunos são bolsista?
+
+const bolsitas = (acumulador, atual) => {
+    return acumulador && atual
+}
+
+const todos_s_bolsitas = alunos.map(a => a.bolsista).reduce(bolsitas)
+console.log(todos_s_bolsitas)
+
+// Desafio 2, algum é bolsista
+
+const bolsistas_2 = (acumulador, atual) => acumulador || atual
+console.log(alunos.map(a => a.bolsista).reduce(bolsistas_2))
+
+// Criando o reduce
+
+Array.prototype.new_reduce = function(callback){
+    let acumulador = this[0]
+    for(i = 1; i < this.length; i++){
+        acumulador = callback(acumulador, this[i])
+    }
+    return acumulador
+}
+
+console.log(alunos.map(a => a.nota).new_reduce(func_callback))
