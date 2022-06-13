9f776f5d2fbf25e3877dfd6bf4e6553f0504ea2a
davi12345452 <davijanischmaia@gmail.com>
2022-06-13 16:51:13 -0300

    update

--- Seção 8 - Array JS/forEach.js
+++ Seção 8 - Array JS/forEach.js
@@ -1,3 +1,31 @@
 /*
- * MÉTODO FOREACH: 
+ * MÉTODO FOREACH: usado para percorrer listas/arrays, usando o callback de uma função,
+ * o forEach é um método em que ele passa por cada elemento de um array e executa uma
+ * função sobre o dado, como se fosse um for in. Por isso as função dentro dele são
+ * chamadas de callback, pois são executadas várias vezes. O forEach é formado por um
+ * laço for
+ * 
+ * O forEach tem uma prioridade, primeiro ele chama o conteúdo, o dado em si, se houver
+ * um segundo chamamento, ele chama o índice. Se o método tiver 1 argumento, ele simples-
+ * mente ignora o índice. O forEach tem 3 parametros: dado, indice e o próprio array, nessa
+ * ordem. Depois desses 3, o argumento fica indefinido
  */
+
+const marcas_carros = ["Mercedes", "Ferrari", "BMW", "Chevrolet", "Toyota"]
+
+marcas_carros.forEach(function(nome, indice){  //O forEach pega o dado e posteriormente o indice, sempre.
+    console.log(`${indice + 1}) ${nome}`)
+})
+
+const nomes = nome => console.log(nome)
+marcas_carros.forEach(nomes) //Detalhe, não usamos () depois do nome da função. marcas_carros.forEach(nomes()) -> ERRO
+
+// Criando o método forEach: função que recebe uma função callback.
+
+Array.prototype.forEach2 = function(funcao_callback){
+    for(i in this){
+        funcao_callback(this[i], i, this) //Devolve dado, indice e array
+    }
+}
+
+marcas_carros.forEach2(nomes)
--- Seção 8 - Array JS/metodo_map.js
+++ Seção 8 - Array JS/metodo_map.js
@@ -1,3 +1,71 @@
 /*
- * MÉTODO MAP: 
+ * MÉTODO MAP: é um método que aplica uma tranformação padrão para todos elementos
+ * do array. Por exemplo, usar map em um array numérico para somar 1 em todos ele-
+ * mentos. O map recebe de argumento uma callback que irá ser usada nos dados, re-
+ * tornando um novo array, tranformado. Vale destacar, que como é uma função a call
+ * back, podemos ter if dentro dela, implicando na tranformação de apenas alguns 
+ * elementos. Assim como a forEach, a função callback do map pode receber 3 argu-
+ * mentos, dado, indice e array, em ordem.
  */
+
+const nums = [1, 2, 3, 5, 6, 7, 8]
+let nums_map = nums.map(num => num + 1)
+console.log(nums_map)
+
+const nova_function = num => {
+    if(num > 3){
+        return num + 2
+    }else{
+        return num + 10
+    }
+}
+
+nums_map = nums.map(nova_function)
+console.log(nums_map)
+
+// Tranformando os elementos do array em binário:
+
+const nums_binario = nums.map(num => num.toString(2))
+console.log(nums_binario)
+
+const nums_binario2 = nums_map.map(num => num.toString(2))
+console.log(nums_binario2)
+
+// O map não altera o array original, somente cria um novo a partir deste e de uma função callback
+
+//Podemos aplica maps sucessivos:
+
+const map_sucessivo = nums.map(num => num.toString(2)).map(nova_function).map(num => num.toString(3)) //Ele vai executando sucessivamente
+console.log(map_sucessivo)
+
+// Desafio: recebe arquivos json e retorna o preço em string, R$ e com vírgula
+
+const json = [
+    '{"nome":"Caderno", "preco": 20.50}',
+    '{"nome":"Estojo", "preco": 30}',
+    '{"nome":"Lapis", "preco":1.40}',
+    '{"nome":"Cola", "preco":5.55}'
+]
+
+// Criar função callback que pega um json e devolve o preço somente
+
+const json_func = texto =>{
+    let obj = JSON.parse(texto)
+    return `R$${obj.preco.toFixed(2).replace(".", ",")}`
+}
+
+const json_to_array = json.map(json_func)
+console.log(json_to_array)
+
+// Criando o método map:
+
+Array.prototype.map2 = function(callback){
+    const newArray = []
+    for(i in this){
+       newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
+
+const novo = nums.map2(nova_function)
+console.log(novo)
