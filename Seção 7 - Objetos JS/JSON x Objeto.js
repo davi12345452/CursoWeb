b997c7a0c44d6bb45b60334c35db1f0e6451273c
davi12345452 <davijanischmaia@gmail.com>
2022-06-12 21:57:26 -0300

    Update

--- Seção 7 - Objetos JS/JSON x Objeto.js
+++ Seção 7 - Objetos JS/JSON x Objeto.js
@@ -1,3 +1,19 @@
 /*
- * JSON vs Objeto:
+ * JSON vs Objeto: JSON é um formato textual que carrega os dados, o objetivo
+ * é ser universal, ou seja, eu poder programar através de js, dar de saída um
+ * objeto em JSON, que vai ser usado sla, em Python. É um formato de texto muito
+ * simples de ser interpretado pelas linguagens, assim temos um formato de dado
+ * universal.
  */
+
+// Transformando um OBJETO em JSON:
+
+const obj = { a:1, b:2, c:3, soma(){return a + b + c}, d:5 }
+console.log(JSON.stringify(obj)) // Vale notar, que funções não são devolvidas no JSON já que ele devolve dados e não execuções.
+
+// Transformando um JSON em OBJETO.js:
+
+const obj_json = JSON.stringify(obj) // -> "{ a:1, b:2}" != {"a":1, "b":2}, segundo é formato JSON (a chave deve estar em aspas duplas)
+console.log(JSON.parse(obj_json)) // A função se exclui na tradução para JSON, logo, no contrário ela não vai aparecer.
+
+// Json suporta arrays e objetos, string deve estar entre aspas, equanto booleano e number não precisa
---
+++ Seção 7 - Objetos JS/classes.js
@@ -0,0 +1,40 @@
+/**
+ * CLASSES: as classes não são muito importantes em js pq ela é uma função no final
+ * e ja usamos funçoes para criar objetos. No fim, é só um jeito diferente de criar
+ * objeto por função. Maneira alternativa para trabalhar com funções construtoras
+ */
+
+class Lancamento {
+    constructor(nome = "generico", valor = 0){
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro{
+    constructor(mes, ano){
+        this.mes = mes
+        this.ano = ano
+        this.Lancamento = []
+    }
+
+    addlancamento(...Lancamento){
+        Lancamento.forEach(l => this.Lancamento.push(l))
+    }
+
+    sumario(){
+        let valorConsolidado = 0
+        this.Lancamento.forEach(l =>{
+            valorConsolidado += l.valor
+        })
+        return valorConsolidado
+    }
+}
+
+const salario = new Lancamento("salario", 30000)
+const conta_de_luz = new Lancamento("luz", -500)
+const conta_de_agua = new Lancamento("agua", -120)
+
+const contas = new CicloFinanceiro(05, 2022)
+contas.addlancamento(salario, conta_de_agua, conta_de_luz)
+console.log(contas.sumario())
