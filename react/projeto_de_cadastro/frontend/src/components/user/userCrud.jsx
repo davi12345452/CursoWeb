e29c5dd39b6052a6129f227b16b629d26419390c
davi12345452 <davijanischmaia@gmail.com>
2022-06-27 17:45:28 -0300

    update

--- react/projeto_de_cadastro/frontend/src/components/user/userCrud.jsx
+++ react/projeto_de_cadastro/frontend/src/components/user/userCrud.jsx
@@ -22,6 +22,12 @@ export default class userCrud extends Component {
 
     state = {...initialState} //Inicializando o initial state
 
+    componentWillMount(){
+        axios(baseURL).then(resp=>{
+            this.setState({ list:resp.data})
+        })
+    }
+
     clear(){ //Limpa, quando o usuário clicar em cancelar
         this.setState({user: initialState.user}) //Não limpa a lista, somente o user.
     }
@@ -52,10 +58,62 @@ export default class userCrud extends Component {
         return list
     }
 
+    updateField(event){
+        const user = {...this.state.user}
+        user[event.target.name] = event.target.value
+        this.setState({user})
+    }
+
+    renderForm(){
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className='form-control' name='name'
+                             value={this.state.user.name}
+                             onChange={e =>this.updateField(e)}
+                             placeholder='Digite o nome...' />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input type="text" className='form-control' name='email'
+                             value={this.state.user.email}
+                             onChange={e =>this.updateField(e)}
+                             placeholder='Digite o email...' />
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary" onClick={e => this.save(e)}>
+                            Salvar</button>
+                        <button className="btn btn-secondary ml-2" onClick={e=>this.clear(e)}>
+                            Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    load(user) {
+        this.setState({user})
+    }
+
+    remove(user){
+        axios.delete(`${baseURL}/${user.id}`).then(resp=>{
+            const list = this.state.list.filter(u=>u !== user)
+            this.setState({list})
+        })
+    }
+
     render() {
         return(
             <Main {...headerProps}>
-                Main
+                {this.renderForm()}
             </Main>
         )
     }
