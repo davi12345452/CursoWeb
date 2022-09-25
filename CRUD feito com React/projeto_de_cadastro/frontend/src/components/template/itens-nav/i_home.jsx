6f2650e25e5b377b23721433aa60e1677b190ef1
davi12345452 <davijanischmaia@gmail.com>
2022-09-25 20:47:28 -0300

    Update

--- CRUD feito com React/projeto_de_cadastro/frontend/src/components/template/itens-nav/i_home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default props =>
-    <a href="/home">
-        <i className="fa fa-home">Início</i>
-    </a>
--- CRUD feito com React/projeto_de_cadastro/frontend/src/components/template/itens-nav/i_users.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default props =>
-    <a href="/users">
-        <i className="fa fa-user">Usuários</i>
-    </a>
